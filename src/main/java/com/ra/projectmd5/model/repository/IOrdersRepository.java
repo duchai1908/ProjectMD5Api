3c31d0d756c4da85cde349bf95c7cb590e17da00
duchai1908 <ndh2912@gmail.com>
2024-10-05 23:10:47 +0700

    order

--- src/main/java/com/ra/projectmd5/model/repository/IOrdersRepository.java
+++ src/main/java/com/ra/projectmd5/model/repository/IOrdersRepository.java
@@ -1,7 +1,17 @@
 package com.ra.projectmd5.model.repository;
 
 import com.ra.projectmd5.model.entity.Orders;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface IOrdersRepository extends JpaRepository<Orders, Long> {
+    @Query("SELECT o FROM Orders o WHERE o.user.id = :userId AND o.code LIKE CONCAT('%', :search, '%')")
+    Page<Orders> getAllOrderByUser(@Param("userId") Long userId, @Param("search") String search, Pageable pageable);
+    Page<Orders> findAllByUserId(Long userId, Pageable pageable);
+    Optional<Orders> findByUserIdAndId(Long userId, Long orderId);
 }
