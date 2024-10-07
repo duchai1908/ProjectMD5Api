12a09a5db1615b92879de95d2b58dccd0464fc13
Duc Anh <137094647+Konta37@users.noreply.github.com>
2024-10-07 15:44:22 +0700

    add

--- src/main/java/com/ra/projectmd5/model/service/impl/ColorServiceImpl.java
+++ src/main/java/com/ra/projectmd5/model/service/impl/ColorServiceImpl.java
@@ -1,11 +1,18 @@
 package com.ra.projectmd5.model.service.impl;
 
+import com.ra.projectmd5.exception.CustomException;
 import com.ra.projectmd5.exception.DataExistException;
 import com.ra.projectmd5.model.entity.Color;
 import com.ra.projectmd5.model.repository.IColorRepository;
+import com.ra.projectmd5.model.repository.IProductDetailRepository;
 import com.ra.projectmd5.model.service.IColorService;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -18,11 +25,12 @@ import java.util.Objects;
 public class ColorServiceImpl implements IColorService {
     private final ModelMapper modelMapper;
     private final IColorRepository colorRepository;
+    private final IProductDetailRepository productDetailRepository;
 
     /**
      * Get All Color
      * Auth: Duc Hai (02/10/2024)
-     * */
+     */
     @Override
     public List<Color> getAllColors() {
         return colorRepository.findAll();
@@ -30,23 +38,25 @@ public class ColorServiceImpl implements IColorService {
 
     /**
      * Get Color By Color Id
+     *
      * @Param {*} id: Color id
      * Auth: Duc Hai (02/10/2024)
-     * */
+     */
     @Override
     public Color getColorById(Long id) {
-        return colorRepository.findById(id).orElseThrow(()-> new NoSuchElementException("Color not found"));
+        return colorRepository.findById(id).orElseThrow(() -> new NoSuchElementException("Color not found"));
     }
 
     /**
      * Add new Color
+     *
      * @Param {*} color: Object Color
      * Auth: Duc Hai (02/10/2024)
-     * */
+     */
     @Override
     public Color save(Color color) throws DataExistException {
-        if(colorRepository.existsByColor(color.getColor())){
-            throw new DataExistException("Color is exists","color");
+        if (colorRepository.existsByColor(color.getColor())) {
+            throw new DataExistException("Color is exists", "color");
         }
         color.setCreated_at(new Date());
         color.setUpdated_at(new Date());
@@ -55,15 +65,16 @@ public class ColorServiceImpl implements IColorService {
 
     /**
      * Update Clor by Id
+     *
      * @Param {*} color: Object Color
      * @Param {*} id: Color id
      * Auth: Duc Hai (02/10/2024)
-     * */
+     */
     @Override
     public Color update(Color color, Long id) throws DataExistException {
 
         Color colors = getColorById(id);
-        if(!Objects.equals(color.getColor(), colors.getColor()) && colorRepository.existsByColor(color.getColor())) {
+        if (!Objects.equals(color.getColor(), colors.getColor()) && colorRepository.existsByColor(color.getColor())) {
             throw new DataExistException("Color  is exists", "color");
         }
         colors.setColor(color.getColor());
@@ -73,12 +84,41 @@ public class ColorServiceImpl implements IColorService {
 
     /**
      * Delete color by color id
+     *
      * @Param {*} id: Color id
      * Auth: Duc Hai (02/10/2024)
-     * */
+     */
     @Override
-    public void delete(Long id) {
-        Color colors = getColorById(id);
-        colorRepository.delete(colors);
+    public void delete(Long id) throws CustomException {
+        if (colorRepository.existsById(id)) {
+            if (productDetailRepository.existsByColorId(id)) {
+                throw new CustomException("Màu này có được dùng trong sản phẩm.", HttpStatus.CONFLICT);
+            } else {
+                colorRepository.deleteById(id);
+            }
+        } else {
+            throw new CustomException("color not found", HttpStatus.NOT_FOUND);
+        }
     }
+
+    @Override
+    public Page<Color> searchColors(Pageable pageable, String color, String sortOption) {
+        Sort sort = switch (sortOption) {
+            case "aToZ" -> Sort.by(Sort.Direction.ASC, "color");
+            case "zToA" -> Sort.by(Sort.Direction.DESC, "color");
+            default -> pageable.getSort();
+        };
+
+        Pageable sortedPageable = PageRequest.of(pageable.getPageNumber(), pageable.getPageSize(), sort);
+
+        Page<Color> colorPage;
+        if (color == null || color.isEmpty()) {
+            colorPage = colorRepository.findAll(sortedPageable);
+        } else {
+            colorPage = colorRepository.findAllByColorContainsIgnoreCase(color, sortedPageable);
+        }
+
+        return colorPage;
+    }
+
 }
